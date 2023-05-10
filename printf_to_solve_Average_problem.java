66773dbce5d42b39ef14b5dab3416a6b09e69f95
Kumaravel K <90759617+KumaravelProgrammer@users.noreply.github.com>
2023-05-10 19:03:02 +0530

    Add files via upload

---
+++ printf_to_solve_Average_problem.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+class Main {
+  
+  public static void main(String args[]) {
+    
+    float a = 1.34536363f;
+     
+    System.out.printf("%.2f" , a);
+    
+    
+    
+  }
+  
+  
+}
