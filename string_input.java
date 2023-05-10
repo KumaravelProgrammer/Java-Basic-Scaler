0345a3cdaf3c2fed4202c3c5aaee96571f69886c
Kumaravel K <90759617+KumaravelProgrammer@users.noreply.github.com>
2023-05-10 18:55:47 +0530

    Add files via upload

---
+++ string_input.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+class Main {
+  
+  public static void main(String args[]) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    
+    // String Name 
+    String name = sc.nextLine();
+    
+    String someString = sc.nextLine();
+    
+    System.out.println(name);
+    System.out.println(someString);
+
+    
+    
+  }
+  
+  
+}
